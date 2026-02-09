5190a9a462a4e08dba2b4ef6b14c0a85cc1008a0
ben-dev-su <206861590+ben-dev-su@users.noreply.github.com>
2026-02-09 16:30:48 +0100

    add: hello world with gemini client

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai.chats import GenerateContentResponse
+
+
 def main():
-    print("Hello from ai-agent!")
+    _ = load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError()
+
+    client: genai.Client = genai.Client(api_key=api_key)
+
+    response: GenerateContentResponse = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Hello World, Gemini",
+    )
+
+    print(response.text)
 
 
 if __name__ == "__main__":
