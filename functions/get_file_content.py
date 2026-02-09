c34e3ec9952d650015c2c5ebecd3c922fecac10d
ben-dev-su <206861590+ben-dev-su@users.noreply.github.com>
2026-02-09 21:17:39 +0100

    add: get_file_content functions - trys to read the contents of a file in a given directory

---
+++ functions/get_file_content.py
@@ -0,0 +1,35 @@
+import os
+
+READ_LIMIT = 10000
+
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
+
+    try:
+        valid_path = (
+            os.path.commonpath([abs_working_dir, target_file]) == abs_working_dir
+        )
+    except ValueError as e:
+        return f"Error: {e}"
+
+    if not valid_path:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(target_file):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(target_file) as file:
+            content = file.read(READ_LIMIT)
+
+            if file.read(1):
+                content += (
+                    f'\n[...File "{file_path}" truncated at {READ_LIMIT} characters]'
+                )
+
+            return content
+
+    except OSError as e:
+        return f"Error: {e}"
