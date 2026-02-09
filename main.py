c5b7a0e873596ff7425f18febea7298615752336
ben-dev-su <206861590+ben-dev-su@users.noreply.github.com>
2026-02-09 18:17:00 +0100

    add: basic user interactivity - parse cli arguments running the script with argparse

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
-from google.genai.chats import GenerateContentResponse
+from google.genai.types import GenerateContentResponse
 
 
-def main():
+def main() -> None:
+
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        prog="ai-agent", description="Chatbot"
+    )
+    parser.add_argument("prompt", type=str, help="User prompt.")
+    args: argparse.Namespace = parser.parse_args()
+
     _ = load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
@@ -14,9 +23,14 @@ def main():
 
     response: GenerateContentResponse = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Hello World, Gemini",
+        contents=args.prompt,
     )
 
+    if response.usage_metadata is None:
+        raise RuntimeError("api request failed")
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
