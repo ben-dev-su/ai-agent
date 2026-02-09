181eee16feb92da19ebf05002a5db58610706836
ben-dev-su <206861590+ben-dev-su@users.noreply.github.com>
2026-02-09 20:41:12 +0100

    add: get file content size from folder - this function gets the content of a specific folder   and prints the content with its file size to the screen - add a test function to manually test it

---
+++ functions/get_files_info.py
@@ -0,0 +1,35 @@
+import os
+
+
+def get_files_info(working_directory, directory=""):
+
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+
+    try:
+        valid_path = (
+            os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        )
+    except ValueError as e:
+        return f"Error: {e}"
+
+    if not valid_path:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+
+    entries = os.listdir(target_dir)
+    str_builder = ""
+    for entry in entries:
+        abs_entry = os.path.join(target_dir, entry)
+        try:
+            entry_size = os.path.getsize(abs_entry)
+        except OSError as e:
+            return f"Error: {e}"
+
+        is_dir = os.path.isdir(abs_entry)
+
+        str_builder += f"- {entry}: file_size={entry_size} bytes, is_dir={is_dir}\n"
+
+    return str_builder
