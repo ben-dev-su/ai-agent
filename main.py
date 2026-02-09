a703042797487321bc172c9d3f1bd2f6a25542e5
ben-dev-su <206861590+ben-dev-su@users.noreply.github.com>
2026-02-09 18:54:11 +0100

    add: verbose cli argument - add "verbose" cli argument - add user prompt to types.Part to keep the context of what is being   talked about (loop needs to be implemented)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 from google.genai.types import GenerateContentResponse
 
 
@@ -11,7 +12,8 @@ def main() -> None:
     parser: argparse.ArgumentParser = argparse.ArgumentParser(
         prog="ai-agent", description="Chatbot"
     )
-    parser.add_argument("prompt", type=str, help="User prompt.")
+    parser.add_argument("prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args: argparse.Namespace = parser.parse_args()
 
     _ = load_dotenv()
@@ -21,16 +23,21 @@ def main() -> None:
 
     client: genai.Client = genai.Client(api_key=api_key)
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
+
     response: GenerateContentResponse = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.prompt,
+        contents=messages,
     )
 
     if response.usage_metadata is None:
         raise RuntimeError("api request failed")
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print(response.text)
 
 
