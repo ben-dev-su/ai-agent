b56c316bf2a2584c9be6c560c9e3afee2f15b450
ben-dev-su <206861590+ben-dev-su@users.noreply.github.com>
2026-02-10 23:12:01 +0100

    add: system prompt and the abiltiy to call functions as well the function declaration for get_files_info

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 
+from call_function import available_functions
+from prompts import system_prompt
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -28,6 +30,9 @@ def main() -> None:
     response: GenerateContentResponse = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
 
     if response.usage_metadata is None:
@@ -38,7 +43,11 @@ def main() -> None:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    print(response.text)
+    if response.function_calls is not None:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
